a63e9120f87e61ada016213249cbc33c33af9270
fhubert1 <f.hubert.712@gmail.com>
2024-02-29 07:47:22 -0500

    Added 03 Javascript day 1 classroom work

---
+++ 03-Javascript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -0,0 +1,29 @@
+// Write Your JavaScript Code Here
+var personName = "Fred";
+var pets = 2;
+var funFact = "I coach youth football";
+
+console.log("My name is " + personName);
+console.log("I have " + pets + " pet(s)");
+console.log("Fun fact: " + funFact);
+
+
+// reassign 
+personName = "Josie";
+console.log("My name is " + personName);
+
+pets = 14;
+console.log("I have " + pets + " pet(s)");
+
+funFact = "She lives on a farm";
+console.log("Fun fact: " + funFact);
+
+// reassign again
+personName = "Anu";
+console.log("My name is " + personName);
+
+pets = 0;
+console.log("I have " + pets + " pet(s)");
+
+funFact = "I do ballroom dancing";
+console.log("Fun fact: " + funFact);
---
+++ 03-Javascript/01-Activities/06-Stu_Primitive-Types/Unsolved/script.js
@@ -0,0 +1,32 @@
+var one = 64;
+var two = "656302";
+var three = false;
+var four = 64.55;
+var five = "Howdy!";
+var six;
+
+// Insert comments to explain what each console log below will log to the console
+// number
+console.log(typeof one); 
+// string
+console.log(typeof two); 
+// boolean
+console.log(typeof three); 
+// number
+console.log(typeof four); 
+// string
+console.log(typeof five);
+// undefined
+console.log(typeof six);
+
+// string
+four = "Hello!";
+// boolean
+five = false;
+// number
+six = 23;
+
+// Insert comments to explain what each console log below will log to the console
+console.log(typeof four);
+console.log(typeof five);
+console.log(typeof six);
